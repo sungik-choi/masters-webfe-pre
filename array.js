f9810b1be85abf4d4f8e2ef700ce5dabe369de16
최성익 <tjddlr92@gmail.com>
2019-12-30 21:15:53 +0900

    배열과 객체연습 미션1. 배열만들기

---
+++ array.js
@@ -0,0 +1,39 @@
+const data = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": {
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+};
+
+const newData = [];
+
+for (let key in data) {
+    const innerData = data[key];
+    for (let key in innerData) {
+        if(typeof innerData[key] === "number") {
+            newData.push(key);
+        }
+    }
+}
+
+console.table(newData);
