9fb8637708487ae68caa2d502f436368842f1f99
Sungik Choi <tjddlr92@gmail.com>
2019-12-31 17:32:52 +0900

    myReduce 함수 제작중

--- myreduce.js
+++ myreduce.js
@@ -1,11 +1,12 @@
 const myReduce = (arr, callback, initialValue) => {
     //여기에 구현
-    callback.forEach(element => {
-        console.log(element);
-    });
+    if(typeof callback !== "function") throw new TypeError("callback must be a function");
+    console.log(callback);
 }
 
-console.log(myReduce(1,[2,3],3));
+myReduce(1,2,function(accumulator, currentValue, currentIndex, array) {
+    return accumulator + currentValue;
+},3);
 // const result = myReduce(arr, (next, prev) => {
 //     ...
 // }, []);
