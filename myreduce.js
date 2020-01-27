228cc5d37f86fa7d5c4f042a8a444f0580bf0eeb
최성익 <tjddlr92@gmail.com>
2020-01-27 13:43:28 +0900

    myReduce 함수 제작
    reduce 공부하면서 그 당시 못풀었던 reduce 함수를 다시 제작했다. 초깃값(없다면 배열 0번째)에 / 누산기와 현잿값을 인수로 받는 콜백함수의 리턴값을 / 계속 더해서 / 다 더한 결과값을 리턴하는 방식. 을 이해했다.

--- myreduce.js
+++ myreduce.js
@@ -1,12 +1,54 @@
 const myReduce = (arr, callback, initialValue) => {
-    //여기에 구현
-    if(typeof callback !== "function") throw new TypeError("callback must be a function");
-    console.log(callback);
-}
+    let accumulator;
+    let currentIndex;
+    if (typeof initialValue === "undefined") {
+        accumulator = arr[0];
+        currentIndex = 1;
+    } else {
+        accumulator = initialValue;
+        currentIndex = 0;
+    }
+    for (currentIndex; currentIndex < arr.length; currentIndex++) {
+        const currentValue = arr[currentIndex];
+        accumulator = callback(accumulator, currentValue);
+    }
+    return accumulator;
+};
 
-myReduce(1,2,function(accumulator, currentValue, currentIndex, array) {
-    return accumulator + currentValue;
-},3);
-// const result = myReduce(arr, (next, prev) => {
-//     ...
-// }, []);
+// const arr = [3];
+
+// const sum = arr.reduce((acc, cur) => {
+//     return acc + cur
+// }, 10);
+
+// console.log(sum);
+
+// const my = myReduce(arr, (acc = 0, cur = 0) => {
+//     return acc + cur
+// });
+
+// console.log(my);
+
+// const flattened = myReduce([
+//         [0, 1],
+//         [2, 3],
+//         [4, 5]
+//     ],
+//     function (accumulator, currentValue) {
+//         return accumulator.concat(currentValue);
+//     },
+//     []
+// );
+
+// console.log(flattened);
+
+// const countedNames = myReduce(['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'], function (allNames, name) {
+//     if (name in allNames) {
+//         allNames[name]++;
+//     } else {
+//         allNames[name] = 1;
+//     }
+//     return allNames;
+// }, {});
+
+// console.log(countedNames);
