fc55b62179e2d1bccfcde869f9d0b5aaedb048f4
최성익 <tjddlr92@gmail.com>
2020-01-27 17:45:55 +0900

    할일관리 프로그램 만들기
    클래스에 익숙해질겸 객체로 만들어봤다. 어떤식으로 값을 주고받는게 더 좋을지 아직은 감이 잘 안선다. 그리고 출력시 사이에만 쉼표를 넣어야 하는데, 코드를 길게하면 가능하기는 한데... 어떻게 하면 간결하게 짤 수 있을지 고민중이라 일단은 그냥 냅둠.

---
+++ todos.js
@@ -0,0 +1,86 @@
+class Todo {
+  constructor() {
+    this.todos = [
+      {
+        name: '자바스크립트 공부하기',
+        tags: ['programming', 'javascript'],
+        status: 'todo',
+        id: 12123123,
+      },
+      {
+        name: '그림 그리기',
+        tags: ['picture', 'favorite'],
+        status: 'doing',
+        id: 312323,
+      },
+      {
+        name: 'iOS 공부하기',
+        tags: ['programming', 'javascript'],
+        status: 'done',
+        id: 212132,
+      },
+    ];
+  }
+
+  show(command) {
+    const todo = this.todos;
+    switch (command) {
+      case 'all':
+        console.log(this.showAll(todo));
+        break;
+      case 'todos':
+        console.log(this.showTodos(todo));
+        break;
+      default:
+        throw new Error(`${command} is not available command.`);
+        break;
+    }
+  }
+
+  showAll(obj) {
+    let str = '';
+    const statusMap = this.sortStatus(obj);
+    str = `현재 상태: `;
+    statusMap.forEach((value, key) => {
+      str += `${key}:${value}개, `;
+    });
+    return str;
+  }
+
+  showTodos(obj) {
+    let str = '';
+    const nameArr = this.sortName(obj);
+    str = `todo 리스트: 총 ${nameArr.length}건: `;
+    nameArr.forEach(name => (str += `'${name}', `));
+    return str;
+  }
+
+  sortStatus(obj) {
+    const status = new Map();
+    for (let key in obj) {
+      const keyStatus = obj[key].status;
+      status.has(keyStatus)
+        ? status.set(keyStatus, status.get(keyStatus)++)
+        : status.set(keyStatus, 1);
+    }
+    return status;
+  }
+
+  sortName(obj) {
+    const name = [];
+    for (let key in obj) {
+      const keyName = obj[key].name;
+      name.push(keyName);
+    }
+    return name;
+  }
+}
+
+const todo = new Todo();
+todo.show('all');
+todo.show('todos');
+todo.show('blahblah');
+
+// $ nodejs todos.js
+// 현재상태 :  todo: 1개, doing:2개, done:4개
+// todo리스트 :  총3건 : ' 자바스크립트 공부하기' , 'iOS공부하기'
